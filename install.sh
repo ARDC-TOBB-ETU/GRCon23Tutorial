ad32858c6ce734f94b70e2144497e7a4ec777ad7
Murat Sever <murat-sever@live.com>
2023-08-20 01:54:12 +0300

    update installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3.sh -b -p "${HOME}/conda"
@@ -7,3 +7,14 @@ source "${HOME}/conda/etc/profile.d/conda.sh"
 conda config --append channels conda-forge
 
 conda create --name GRCon23 --file requirements.txt
+
+# cd /etc/modprobe.d/
+# sudo gedit blacklist-rtl.conf
+# append: blacklist dvb_usb_rtl28xxu
+
+# echo "blacklist dvb_usb_rtl28xxu" >> /etc/modprobe.d/blacklist.conf
+
+# update-initramfs -u
+
+# sudo apt-get install -y librtlsdr-dev
+
